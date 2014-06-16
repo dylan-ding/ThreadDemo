78e58fc07b5bf58cfd16693dfa4018beea9c052d
dylanding <dding.china@gmail.com>
2014-06-16 02:32:50 -0700

    yes, this is my first multi-thread demo!

---
+++ ThreadDemo001/ThreadDemo001/ThreadDemo001.cpp
@@ -0,0 +1,14 @@
+﻿// ThreadDemo001.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"大家好，这里是测试多线程的演示程序！"<<endl;
+	return 0;
+}
+
